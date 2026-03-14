14236a685936dd324497e70d2f1930e4d005c2ef
rabby775 <rabby775islam@gmail.com>
2026-03-14 21:02:08 +0600

    Resolve timezone issues in date handling

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-14T15:01:28.457Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-14T15:02:07.962Z
+Commit: Resolve timezone issues in date handling
